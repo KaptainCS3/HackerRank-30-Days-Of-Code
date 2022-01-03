930ec9a6e661dae82678f4f7673158ae1a96effa
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-03 23:13:50 +0100

    reading string big not solved

--- Day 1_DataTypes/DataTypes.c
+++ Day 1_DataTypes/DataTypes.c
@@ -10,7 +10,7 @@ int main() {
 
     
     // Declare second integer, double, and String variables.
-    int x = 2; double y = 2.0; char str[] = "is the best place to learn and practice coding!";
+    int x; double y; char str[105];
     // Read and save an integer, double, and String to your variables.
     scanf("%d %lf %s", &x, &y, s);
     // Print the sum of both integer variables on a new line.
@@ -19,6 +19,7 @@ int main() {
     printf("%.1lf\n", d + y);
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
-    printf("%s%s", s, str);
+    printf("%s", s);
+    printf("%s", str);
     return 0;
 }
